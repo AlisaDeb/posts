30afbbecdd3eaefff1b2ecabd2d11bcd950a526f
Alisa Debich <alisashd8@gmail.com>
2024-09-04 14:59:53 +0200

    remove react fragment from Posts

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -4,12 +4,10 @@ import * as SC from './styles';
 
 export const Posts = ({ posts }) => {
   return (
-    <>
-      <SC.Posts>
-        {posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}{' '}
-      </SC.Posts>
-    </>
+    <SC.Posts>
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}{' '}
+    </SC.Posts>
   );
 };
